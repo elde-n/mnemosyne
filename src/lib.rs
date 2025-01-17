14da2bc249984b835006f68cb0570990659e10ae
elde-n <elden_@tutanota.com>
2025-01-17 09:43:17 +0200

    add process module

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 pub mod detour;
 #[cfg(feature = "module")]
 pub mod module;
+#[cfg(feature = "process")]
+pub mod process;
 #[cfg(feature = "scanner")]
 pub mod scanner;
 #[cfg(feature = "vtable")]
---
+++ src/process.rs
@@ -0,0 +1,73 @@
+use std::{
+    ffi::OsStr,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+pub struct Process {
+    pub id: u64,
+    pub base: u64,
+    pub name: String,
+}
+
+fn process_from_path(path: PathBuf) -> Option<Process> {
+    if !path.is_dir() {
+        return None;
+    }
+
+    let Some(file_name) = path.file_name().and_then(OsStr::to_str) else {
+        return None;
+    };
+
+    let Ok(id) = file_name.parse::<u64>() else {
+        return None;
+    };
+
+    let Ok(name) = std::fs::read_to_string(path.join("comm")) else {
+        return None;
+    };
+
+    let base = {
+        let Ok(maps_file) = std::fs::File::open(path.join("maps")) else {
+            return None;
+        };
+
+        let mut reader = BufReader::new(maps_file);
+        let mut line = String::new();
+        reader.read_line(&mut line).ok()?;
+
+        let Some(Ok(base)) = line
+            .split_once('-')
+            .and_then(|(base, _)| Some(base.parse::<u64>()))
+        else {
+            return None;
+        };
+
+        base
+    };
+
+    Some(Process {
+        id,
+        base,
+        name: name.trim().to_string(),
+    })
+}
+
+pub fn list() -> std::io::Result<Vec<Process>> {
+    let mut processes = vec![];
+    for entry in std::fs::read_dir("/proc")? {
+        let path = entry?.path();
+        let process = process_from_path(path);
+
+        if let Some(process) = process {
+            processes.push(process)
+        }
+    }
+
+    Ok(processes)
+}
+
+pub fn from_id(id: u64) -> Option<Process> {
+    let path = PathBuf::from(format!("/proc/{}", id.to_string()));
+    process_from_path(path)
+}
